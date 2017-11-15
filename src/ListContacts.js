b9015f8aa6454b7cfc059f4bee1b4d1cd8f1d161
Yue Yang <shoominga@gmail.com>
2017-11-14 23:36:49 -0800

    first commit

---
+++ src/ListContacts.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+function ListContacts (props) {
+  return (
+    <ol className='contact-list'>
+      {props.contacts.map((contact) => (
+        <li key={contact.id} className='contact-list-item'>
+          <div className='contact-avatar' style={{
+            backgroundImage: `utl(${contact.avatarURL})`
+          }}>
+          </div>
+          <div className='contact-details'>
+            <p>{contact.name}</p>
+            <p>{contact.email}</p>
+          </div>
+          <button onClick={() => props.onDeleteContact(contact)} className='contact-remove'>
+            Remove
+          </button>
+        </li>
+      ))}
+    </ol>
+  )
+}
+
+ListContacts.PropTypes = {
+  contacts: PropTypes.array.isRequired,
+  onDeleteContact: PropTypes.func.isRequired
+}
+
+export default ListContacts
